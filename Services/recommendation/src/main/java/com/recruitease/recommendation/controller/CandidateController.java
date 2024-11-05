5039014c76df6f6f89a84fe105c46b516320e12c
Ravishan Jayathilake <jayathilakeravishan@gmail.com>
2024-11-05 12:34:16 +0000

    autorization

--- Services/recommendation/src/main/java/com/recruitease/recommendation/controller/CandidateController.java
+++ Services/recommendation/src/main/java/com/recruitease/recommendation/controller/CandidateController.java
@@ -1,21 +1,41 @@
 package com.recruitease.recommendation.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import com.recruitease.recommendation.DTO.CandidateRequest;
-import java.util.List;
-import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.recruitease.recommendation.DTO.ApplicationResponse;
+import com.recruitease.recommendation.clients.ApplicationServiceConsumer;
+import com.recruitease.recommendation.service.CandidateRecommendation;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
-@RequestMapping("/api/candidaterecommendation")
+@RequestMapping("/api/v1/recommendations")
 public class CandidateController {
 
+    private final CandidateRecommendation rankingService;
+    private final ApplicationServiceConsumer applicationServiceConsumer;
+
     @Autowired
-    private CandidateRankingService rankingService;
-
-    @PostMapping("/rank")
-    public Map<String, Double> rankCandidates(@RequestBody CandidateRequest request) {
-        return rankingService.rankCandidatesByKeywords(request.getCandidates(), request.getKeywords());
+    public CandidateController(CandidateRecommendation rankingService, ApplicationServiceConsumer applicationServiceConsumer) {
+        this.rankingService = rankingService;
+        this.applicationServiceConsumer = applicationServiceConsumer;
     }
+
+    // @PostMapping("/rank")
+    // public Map<String, Double> rankCandidates(@RequestBody CandidateRequest request) {
+    //     return rankingService.rankCandidatesByKeywords(request.getCandidates(), request.getKeywords());
+    // }
+
+    HttpServletRequest request;
+    String token = request.getHeader("Authorization");
+
+    @GetMapping("/rank")
+    public ApplicationResponse rankCandidates() {
+        return applicationServiceConsumer.getApplicationResponse(token);
+    }
+    
+
 }
-
-
--- Services/recommendation/src/main/java/com/recruitease/recommendation/service/CandidateRecommendation.java
+++ Services/recommendation/src/main/java/com/recruitease/recommendation/service/CandidateRecommendation.java
@@ -1,62 +1,55 @@
 package com.recruitease.recommendation.service;
-import org.apache.tika.Tika;
-import org.apache.tika.exception.TikaException;
 import org.springframework.stereotype.Service;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 public class CandidateRecommendation {
 
-    private final Tika tika = new Tika();
+    // private final Tika tika = new Tika();
 
   
-    public Map<String, Double> rankCandidatesByKeywords(List<Candidate> candidates, List<String> keywords) {
-        Map<String, Double> candidateScores = new HashMap<>();
+    // public Map<String, Double> rankCandidatesByKeywords(List<Candidate> candidates, List<String> keywords) {
+    //     Map<String, Double> candidateScores = new HashMap<>();
 
-        for (Candidate candidate : candidates) {
-            String combinedText = combineProfileAndCvText(candidate);
-            double score = calculateKeywordScore(combinedText, keywords);
-            candidateScores.put(candidate.getCandidateId(), score);
-        }
+    //     for (Candidate candidate : candidates) {
+    //         String combinedText = combineProfileAndCvText(candidate);
+    //         double score = calculateKeywordScore(combinedText, keywords);
+    //         candidateScores.put(candidate.getCandidateId(), score);
+    //     }
 
-        return candidateScores.entrySet()
-                .stream()
-                .sorted(Map.Entry.<String, Double>comparingByValue().reversed())
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
-    }
+    //     return candidateScores.entrySet()
+    //             .stream()
+    //             .sorted(Map.Entry.<String, Double>comparingByValue().reversed())
+    //             .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+    // }
 
-    private String combineProfileAndCvText(Candidate candidate) {
-        StringBuilder combinedText = new StringBuilder();
+    // private String combineProfileAndCvText(Candidate candidate) {
+    //     StringBuilder combinedText = new StringBuilder();
 
-        combinedText.append(Optional.ofNullable(candidate.getSkills()).orElse(""))
-                    .append(" ")
-                    .append(Optional.ofNullable(candidate.getAboutMe()).orElse(""))
-                    .append(" ")
-                    .append(Optional.ofNullable(candidate.getExperience()).orElse(""))
-                    .append(" ")
-                    .append(Optional.ofNullable(candidate.getEducation()).orElse(""));
+    //     combinedText.append(Optional.ofNullable(candidate.getSkills()).orElse(""))
+    //                 .append(" ")
+    //                 .append(Optional.ofNullable(candidate.getAboutMe()).orElse(""))
+    //                 .append(" ")
+    //                 .append(Optional.ofNullable(candidate.getExperience()).orElse(""))
+    //                 .append(" ")
+    //                 .append(Optional.ofNullable(candidate.getEducation()).orElse(""));
 
       
-        if (candidate.getCvFilePath() != null) {
-            try {
-                File cvFile = new File(candidate.getCvFilePath());
-                combinedText.append(" ").append(tika.parseToString(cvFile));
-            } catch (IOException | TikaException e) {
-                e.printStackTrace();
-            }
-        }
+    //     if (candidate.getCvFilePath() != null) {
+    //         try {
+    //             File cvFile = new File(candidate.getCvFilePath());
+    //             combinedText.append(" ").append(tika.parseToString(cvFile));
+    //         } catch (IOException | TikaException e) {
+    //             e.printStackTrace();
+    //         }
+    //     }
 
-        return combinedText.toString();
-    }
+    //     return combinedText.toString();
+    // }
 
-    private double calculateKeywordScore(String text, List<String> keywords) {
-        String[] words = text.toLowerCase().split("\\W+");
-        long totalWords = words.length;
-        long keywordMatches = Arrays.stream(words).filter(keywords::contains).count();
-        return (double) keywordMatches / totalWords * 100;
-    }
+    // private double calculateKeywordScore(String text, List<String> keywords) {
+    //     String[] words = text.toLowerCase().split("\\W+");
+    //     long totalWords = words.length;
+    //     long keywordMatches = Arrays.stream(words).filter(keywords::contains).count();
+    //     return (double) keywordMatches / totalWords * 100;
+    // }
 }
