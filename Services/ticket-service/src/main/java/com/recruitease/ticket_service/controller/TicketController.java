dd09c621b8aeaa92be8baad46ae3a2ce46296bce
chathura226 <chathuralakshan226@gmail.com>
2024-11-18 05:17:33 +0530

    basic configs,controller,dto and entity changes

--- Services/ticket-service/src/main/java/com/recruitease/ticket_service/controller/TicketController.java
+++ Services/ticket-service/src/main/java/com/recruitease/ticket_service/controller/TicketController.java
@@ -3,6 +3,7 @@ package com.recruitease.ticket_service.controller;
 import com.recruitease.ticket_service.dto.TicketDTO;
 import com.recruitease.ticket_service.dto.request.TicketUpdateDTO;
 import com.recruitease.ticket_service.service.TicketService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,11 +11,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/ticket")
-@CrossOrigin
+@RequiredArgsConstructor
 public class TicketController {
 
-    @Autowired
-    private TicketService ticketService;
+    private final TicketService ticketService;
 
     @PostMapping(
             path = {"/save"}
--- Services/ticket-service/src/main/java/com/recruitease/ticket_service/entity/Ticket.java
+++ Services/ticket-service/src/main/java/com/recruitease/ticket_service/entity/Ticket.java
@@ -4,39 +4,78 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Table(schema = "ticket")
+@EntityListeners(AuditingEntityListener.class) //since we need to track created and modified dates
+@Table(name = "ticket")
 public class Ticket {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ticket_id", nullable = false)
-    private int ticketId;
-
-    @Column(name = "delay", length = 60, nullable = false)
-    private String delay;
-
-    @Column(name = "subject", length = 60, nullable = false)
+    private String ticketId;
+    @Column(name = "type", length = 50)
+    private TicketType type;
+    @Column(name = "status", length = 50)
+    private TicketStatus status;
     private String subject;
-
-    @Column(name = "type", length = 60, nullable = false)
-    private String type;
-
-    @Column(name = "job_title", length = 100, nullable = false)
-    private String jobTitle;
-
-    @Column(name = "date", length = 45, nullable = false)
-    private String date;
-
-    @Column(name = "status", length = 60, nullable = false)
-    private String status;
-
-    @Column(name = "name", length = 60, nullable = false)
-    private String name;
-
-    @Column(name = "description", length = 100, nullable = false)
     private String description;
+    private String note; //status change note
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    public enum TicketStatus {
+        RESOLVED,
+        REJECTED,
+        UNDER_REVIEW,
+    }
+
+    public enum TicketType {
+        JOB_OFFER,
+        JOB_APPLICATION,
+        JOB_LISTING,
+        INTERVIEW,
+        PAYMENT,
+        OTHER,
+        SUPPORT
+    }
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "ticket_id", nullable = false)
+//    private int ticketId;
+//
+//    @Column(name = "delay", length = 60, nullable = false)
+//    private String delay;
+//
+//    @Column(name = "subject", length = 60, nullable = false)
+//    private String subject;
+//
+//    @Column(name = "type", length = 60, nullable = false)
+//    private String type;
+//
+//    @Column(name = "job_title", length = 100, nullable = false)
+//    private String jobTitle;
+//
+//    @Column(name = "date", length = 45, nullable = false)
+//    private String date;
+//
+//    @Column(name = "status", length = 60, nullable = false)
+//    private String status;
+//
+//    @Column(name = "name", length = 60, nullable = false)
+//    private String name;
+//
+//    @Column(name = "description", length = 100, nullable = false)
+//    private String description;
 }
