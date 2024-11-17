2234af8c4169b153e5d037b20ad4c838ab6c0e92
chathura226 <chathuralakshan226@gmail.com>
2024-11-18 05:29:00 +0530

    service and dto breif

--- Services/ticket-service/src/main/java/com/recruitease/ticket_service/repository/TicketRepo.java
+++ Services/ticket-service/src/main/java/com/recruitease/ticket_service/repository/TicketRepo.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@EnableJpaRepositories
-public interface TicketRepo extends JpaRepository<Ticket, Integer> {
+public interface TicketRepo extends JpaRepository<Ticket, String> {
 }
